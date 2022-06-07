75d5616fbb0abe78cda114bde92fcfacb76e8fe7
AdarshVMore <103880462+AdarshVMore@users.noreply.github.com>
2022-06-07 17:26:24 +0530

    Add files via upload
    all files pushed

---
+++ script.js
@@ -0,0 +1,8 @@
+let scroll = document.getElementById('scroll')
+// let scrollbar = document.getElementById('bgc')
+
+scroll.onclick = function (){
+    scroll.classList.toggle('scroll-toggle');
+    // scrollbar.classList.toggle('bgc-toggle')
+    
+}
